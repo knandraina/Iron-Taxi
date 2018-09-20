e98b848c6099b3755d8f2264cf322612877040da
Kevin Rabesaotra <kevin.rabesaotra@gmail.com>
2018-09-20 08:50:27 +0200

    if username & password is empty message error

--- routes/authentification.js
+++ routes/authentification.js
@@ -3,8 +3,20 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 
-router.get('/login', (req, res, next) => {
+router.post('/login', (req, res, next) => {
+    const {username, password} = req.body
+    console.log(username, password);
+    if (username ==='' || password === '') {
+        res.render("index", {
+            errorMessage : 'Indicate a username and a password to sign up'
+        });
+        return
+    }
     res.render('authentification/login')
+})
+
+router.get('/register', (req, res, next) => {
+    res.render('authentification/register')
 })
 
 module.exports = router;
